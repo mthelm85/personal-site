f35268bd709e9fe1d26e111609856852629a2e38
Matt Helm <mthelm85@gmail.com>
2021-07-09 17:19:24 -0500

    changed to SPA and configured for GitHub Pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,7 +28,10 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
   ],
-
+  mode: 'spa',
+  router: {
+     base: '/personal-site/'
+   },
   target: 'static',
 
   // Auto import components: https://go.nuxtjs.dev/config-components
